6f4c546437271ecbf817d8aa9c29a19702806d48
Fabian Diet <fabian.diet@spaceapplications.com>
2022-10-09 15:49:53 +0200

    Add willReadFrequently hint to hit canvas

--- src/graphics/HitCanvas.ts
+++ src/graphics/HitCanvas.ts
@@ -34,7 +34,11 @@ export class HitCanvas {
         const canvas = document.createElement('canvas');
         canvas.width = width || canvas.width;
         canvas.height = height || canvas.height;
-        this.ctx = canvas.getContext('2d')!;
+        this.ctx = canvas.getContext('2d', {
+            // Hint to use a software canvas, instead of hardware-accelerated.
+            // Recommended due to frequent getImageData().
+            willReadFrequently: true,
+        })!;
 
         // Find the root
         let candidate = parent;
